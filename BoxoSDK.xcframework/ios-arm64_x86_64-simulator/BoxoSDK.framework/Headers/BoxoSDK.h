1003b6d0a20da71f8750ccb0a8991adb1094bf7d
Azamat Kushmanov <azamat.kushmanov@gmail.com>
2025-08-25 11:41:46 +0000

    v0.0.1

---
+++ BoxoSDK.xcframework/ios-arm64_x86_64-simulator/BoxoSDK.framework/Headers/BoxoSDK.h
@@ -0,0 +1,18 @@
+//
+//  BoxoSDK.h
+//  BoxoSDK
+//
+//  Created by Azamat Kushmanov on 19/5/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BoxoSDK.
+FOUNDATION_EXPORT double BoxoSDKVersionNumber;
+
+//! Project version string for BoxoSDK.
+FOUNDATION_EXPORT const unsigned char BoxoSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BoxoSDK/PublicHeader.h>
+
+
